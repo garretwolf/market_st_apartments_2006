ed199a67dadefb4f8d49776705cad9a60ee2aaaa
Garret Wolf <garretwolf26@gmail.com>
2020-07-14 11:15:53 -0400

    Create apartment class and pass first 2 tests

---
+++ lib/apartment.rb
@@ -0,0 +1,10 @@
+class Apartment
+
+attr_reader :number, :monthly_rent, :bathrooms, :bedrooms
+def initialize(info)
+  @number = info[:number]
+  @monthly_rent = info[:monthly_rent]
+  @bathrooms = info[:bathrooms]
+  @bedrooms = info[:bedrooms]
+end
+end

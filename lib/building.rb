00e2719518532063b51e57d7afec5943066e8279
Garret Wolf <garretwolf26@gmail.com>
2020-07-14 12:16:09 -0400

    Finish testing up to add renters to units

--- lib/building.rb
+++ lib/building.rb
@@ -1,11 +1,18 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :renters
 
   def initialize
     @units = []
+    @renters = []
   end
 
   def add_unit(unit)
     @units << unit
   end
+
+  def renters
+    @units.map do |unit|
+      unit.renter.name
+  end
+end
 end
